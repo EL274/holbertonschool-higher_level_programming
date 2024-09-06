6b0990c9fa935494615a6a67376ec648bdda31cf
EL274 <tresormakosso510@gmail.com>
2024-09-06 07:54:04 +0200

    print different combinaison

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 89 and i / 10 < i % 10:
+    if i != 99:
         print("{:02}".format(i), end=",")
-    elif i is 89:
+    else:
         print("{:02}".format(i))
