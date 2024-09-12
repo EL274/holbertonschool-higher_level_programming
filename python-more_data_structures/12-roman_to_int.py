5a2a30030e3b9d36f538339efa431172e4b1e6ed
Maxime <martinsmaxime3@gmail.com>
2024-09-12 12:19:38 +0200

    roman integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     # Vérification de la validité de l'entrée
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     # Dictionnaire des valeurs des chiffres romains
     roman_to_int_map = {
         'I': 1,
@@ -14,23 +14,22 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    
+
     total = 0
     prev_value = 0
-    
+
     # Parcours de la chaîne de droite à gauche
     for char in reversed(roman_string):
         if char not in roman_to_int_map:
             return 0  # Si un caractère invalide est trouvé, retourne 0
-        
+
         value = roman_to_int_map[char]
-        
+
         if value < prev_value:
             total -= value
         else:
             total += value
-        
-        prev_value = value
-    
-    return total
 
+        prev_value = value
+
+    return total
