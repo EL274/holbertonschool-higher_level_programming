84cbb45d3e70bc56804a8759126622735132530f
EL274 <tresormakosso510@gmail.com>
2024-09-13 10:40:43 +0200

    safe

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
         count = 0
             printed = 0
-                
+
                     while printed < x:
                                 try:
                                                 print(my_list[count], end=' ')
@@ -9,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
                                                                         printed += 1
                                                                                 except IndexError:
                                                                                                 break
-                                                                                                
+
                                                                                                 print()
                                                                                                     return printed
 
