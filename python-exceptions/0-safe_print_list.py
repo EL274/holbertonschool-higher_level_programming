56882db69a92d5b6057e99a2e475c50fdafc5cb2
EL274 <tresormakosso510@gmail.com>
2024-09-13 10:35:06 +0200

    safe

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+def safe_print_list(my_list=[], x=0):
+        count = 0
+            printed = 0
+                
+                    while printed < x:
+                                try:
+                                                print(my_list[count], end=' ')
+                                                            count += 1
+                                                                        printed += 1
+                                                                                except IndexError:
+                                                                                                break
+                                                                                                
+                                                                                                print()
+                                                                                                    return printed
+
