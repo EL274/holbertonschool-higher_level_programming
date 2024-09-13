5c9a9f9fea835b5e86307a9ef02c9fcee53ab689
EL274 <tresormakosso510@gmail.com>
2024-09-13 08:15:38 +0200

    divisible

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,5 @@
 divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
 
 my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-i = 0
-while i < len(list_result):
-    # Utiliser {:s} pour une chaîne et conditionner correctement le texte
-    print("{:d} is {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1  # Incrémentation ici est correcte
+for i, res in enumerate(divisible_by_2(my_list)):
+    print(f"{my_list[i]} is {'is' if res else 'is not'} divisible by 2")
