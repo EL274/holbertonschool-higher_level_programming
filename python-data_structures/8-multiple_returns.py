490d67c86506d514f89f6cd301b2ceadbcd832a7
EL274 <tresormakosso510@gmail.com>
2024-09-13 07:46:56 +0200

    multiple return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,5 @@
-
-eef (sentence):
-        if len(sentence) == 0:
-                    return (0, None)
-                    else:
-                                return (len(sentence), sentence[0])
-                            ultiple_returns(sentence):
+#!/usr/bin/python3
+def multiple_returns(sentence):
     if len(sentence) == 0:
         return (0, None)
     else:
