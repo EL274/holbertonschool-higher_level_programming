452c63c7fd8f95c1e440e0371679734dad6b26f6
EL274 <tresormakosso510@gmail.com>
2024-09-13 13:51:29 +0200

    safe division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 def safe_print_division(a, b):
-        try:
-                    result = a / b
-                        except ZeroDivisionError:
-                                    result = None
-                                        except Exception:
-                                                    result = None
-                                                        finally:
-                                                                    print("Inside result: {}".format(result))
-                                                                            return result
-
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    except Exception:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
