512c73d3933a6d8fa3dbc60d76fa12d362c83e57
EL274 <tresormakosso510@gmail.com>
2024-09-13 08:07:29 +0200

    divisible

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -6,5 +6,6 @@ list_result = divisible_by_2(my_list)
 
 i = 0
 while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1  # Incrémentation doit être ici
+    # Utiliser {:s} pour une chaîne et conditionner correctement le texte
+    print("{:d} is {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1  # Incrémentation ici est correcte
