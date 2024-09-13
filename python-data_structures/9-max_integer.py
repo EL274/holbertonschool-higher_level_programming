45f3dafd80115a06d39648269e1cedc71a61dd8f
EL274 <tresormakosso510@gmail.com>
2024-09-13 07:53:10 +0200

    max integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,15 +1,15 @@
+#!/usr/bin/python3
 def max_integer(my_list=[]):
-        if len(my_list) == 0:
-                    return None
+    if len(my_list) == 0:
+        return None
 
-                    # On suppose que le premier nombre est le plus grand
-                        largest = my_list[0]
+    # On suppose que le premier nombre est le plus grand
+    largest = my_list[0]
 
-                            # On vérifie chaque nombre dans la liste
-                                for number in my_list:
-                                            # Si on trouve un nombre plus grand, on le remplace
-                                                    if number > largest:
-                                                                    largest = number
+    # On vérifie chaque nombre dans la liste
+    for number in my_list:
+        # Si on trouve un nombre plus grand, on le remplace
+        if number > largest:
+            largest = number
 
-                                                                        return largest
-
+    return largest
