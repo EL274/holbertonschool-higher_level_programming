2f33a8618702cc60415afc7f8481843ff9585790
EL274 <tresormakosso510@gmail.com>
2024-09-13 07:34:48 +0200

    add

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Si le tuple_a a moins de deux éléments, on le complète avec des zéros
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 1:  # Si tuple_a a un seul élément
-            tuple_a = (tuple_a[0], 0)  # On ajoute un 0 en deuxième position
-        else:  # Si tuple_a est vide
-            tuple_a = (0, 0)  # On met deux zéros
+    # On complète les tuples avec des zéros si nécessaire
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
-    # Si le tuple_b a moins de deux éléments, on le complète avec des zéros
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 1:  # Si tuple_b a un seul élément
-            tuple_b = (tuple_b[0], 0)  # On ajoute un 0 en deuxième position
-        else:  # Si tuple_b est vide
-            tuple_b = (0, 0)  # On met deux zéros
-
-    # On additionne le premier élément de chaque tuple et le deuxième élément de chaque tuple
-    sum_first_elements = tuple_a[0] + tuple_b[0]
-    sum_second_elements = tuple_a[1] + tuple_b[1]
-
-    # On retourne un nouveau tuple avec les résultats des additions
-    return (sum_first_elements, sum_second_elements)
+    # On additionne les éléments correspondants
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
