3512481cbc372fd7f9519ee0e2a2a2e5fdc4afae
EL274 <tresormakosso510@gmail.com>
2024-09-13 08:13:15 +0200

    divisible

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,6 @@ divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
 
 my_list = [0, 1, 2, 3, 4, 5, 6]
 list_result = divisible_by_2(my_list)
-
 i = 0
 while i < len(list_result):
     # Utiliser {:s} pour une chaîne et conditionner correctement le texte
