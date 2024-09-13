cc2ffb043929263572299c3aa5620ee7bc30831a
EL274 <tresormakosso510@gmail.com>
2024-09-13 14:49:14 +0200

    list  division

--- python-exceptions/4-main.py
+++ python-exceptions/4-main.py
@@ -1,4 +1,5 @@
-st_division = __import__('4-list_division').list_division
+#!/usr/bin/python3
+list_division = __import__('4-list_division').list_division
 
 my_l_1 = [10, 8, 4]
 my_l_2 = [2, 4, 4]
@@ -11,4 +12,3 @@ my_l_1 = [10, 8, 4, 4]
 my_l_2 = [2, 0, "H", 2, 7]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
 print(result)
-
