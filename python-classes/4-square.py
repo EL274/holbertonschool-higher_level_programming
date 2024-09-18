200aff74c8778f9e8175aa406c9133fd4ab029de
EL274 <tresormakosso510@gmail.com>
2024-09-18 15:35:26 +0200

    square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
+"""creates class Square with
+private instance attribute size and public instance method"""
+
+
 class Square:
-"""defines class with instantiated and validated private instance attribute
+    """defines class with instantiated and validated private instance attribute
 and public instance method."""
 
     def __init__(self, size=0):
         self.__size = size
 
     @property
-def size(self):
+    def size(self):
         return(self.__size)
 
     @size.setter
-    def __init__(self, size=0):
-        if type(size) is not int:
+    def size(self, value):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
