8f423bae2b99d4bc0b6c6a73b01ce69c9cc7887a
EL274 <tresormakosso510@gmail.com>
2024-09-23 15:25:58 +0200

    delete

--- python-inheritance/0-lookup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""define a function which returns the list of available attributes and methods of an object"""
-
-def lookup(obj):
-    """ returns the list of avaible attributes and methods of an object"""
-
-
-    return dir((obj))
