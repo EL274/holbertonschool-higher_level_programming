c093a31708e35247089a25860e10e8b3096c2c5f
EL274 <tresormakosso510@gmail.com>
2024-09-24 03:01:28 +0200

    mines characters

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@ that is a subclass of the specified class"""
 
 def inherits_from(obj, a_class):
     """
-    Returns True if obj is an instance of a class that inherited the specified class; otherwise False"""
+    Returns True if obj is an instance of a class that inherited the specified class"""
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
