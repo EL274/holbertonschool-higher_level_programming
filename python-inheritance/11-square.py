a2105598405b8660f86ccbf80b7506b6f592d14f
EL274 <tresormakosso510@gmail.com>
2024-09-25 16:59:54 +0200

    square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""Defines class Square that inherits from Rectangle"""
+"""defines class Square that inherits from Rectangle"""
 
 
-Rectangle = _import_('9-rectangle').Rectangle
+Rectangle = __import__('8-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class for Square that inherits from Rectangle
-    with methods for area and string representation."""
-
+    """class for square that inherits from Rectangle
+    with method for area and string representation"""
     def __init__(self, size):
-        """Initializes Square instance"""
-        self.__size = self.integer_validator(size)
-
-    def integer_validator(self, value):
-        """Validates that value is a positive integer"""
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError("size must be a positive integer")
-        return value
+        """initializes Square instance"""
+        self.integer_validator("size", size)
+        self.__size = size
 
     def area(self):
-        """Returns the area of the square"""
-        return self.__size ** 2
+        """returns area of square"""
+        return (self.__size * self.__size)
 
     def __str__(self):
-        """String representation of Square"""
-        return f"[Square] {self.__size}/{self.__size}"
+        """string representation of Square"""
+        str_rep = "[Square] " + str(self.__size) + "/" + str(self.__size)
+        return str_rep
