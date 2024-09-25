8a8d9598b68ec293d6978563270c10c42cfa942c
EL274 <tresormakosso510@gmail.com>
2024-09-25 11:04:13 +0200

    square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""defines class Square that inherits from Rectangle"""
+"""Defines class Square that inherits from Rectangle"""
 
 
-class Square:
-     """class for square that inherits from Rectangle
-    with method for area and string representation"""
-     
+
+class Square(Rectangle):
+    """Class for Square that inherits from Rectangle
+    with methods for area and string representation."""
+    
     def __init__(self, size):
-        """initializes Square instance"""
+        """Initializes Square instance"""
         self.__size = self.integer_validator(size)
 
     def integer_validator(self, value):
+        """Validates that value is a positive integer"""
         if not isinstance(value, int) or value <= 0:
             raise ValueError("size must be a positive integer")
         return value
 
     def area(self):
-        """returns area of square"""
+        """Returns the area of the square"""
         return self.__size ** 2
 
     def __str__(self):
-        """string representation of Square"""
+        """String representation of Square"""
         return f"[Square] {self.__size}/{self.__size}"
-
-    def print(self):
-        print(self.__str__())
