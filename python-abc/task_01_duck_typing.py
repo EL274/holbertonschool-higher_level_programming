f551189da815c3ae630c6d6606af4eb0ecf186b6
EL274 <tresormakosso510@gmail.com>
2024-09-25 14:48:29 +0200

    abstraite

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+# Classe abstraite Animal
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+# Sous-classe Dog qui hérite de Animal
+class Dog(Animal):
+    
+    def sound(self):
+        return "Bark"
+
+# Sous-classe Cat qui hérite de Animal
+class Cat(Animal):
+    
+    def sound(self):
+        return "Meow"
+
+# Exemple d'utilisation
+dog = Dog()
+cat = Cat()
+
+print(dog.sound())  # Affiche "Bark"
+print(cat.sound())  # Affiche "Meow"
+
