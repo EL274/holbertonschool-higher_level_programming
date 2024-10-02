6f54a3028e0a3279c0b590b422a03c1d499ed854
EL274 <tresormakosso510@gmail.com>
2024-10-02 15:44:30 +0200

    modified

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that writes text to a file and returns  number""" 
 
+
 def write_file(filename="", text=""):
 
     with open(filename, "w", encoding="utf-8") as file:
