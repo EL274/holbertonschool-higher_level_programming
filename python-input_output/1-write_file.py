319a030cc5037bb3b497d84f71cac6cda4b9713d
EL274 <tresormakosso510@gmail.com>
2024-10-02 15:21:25 +0200

    write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""defines function that counts number of lines read from file"""
+
+
+
 def write_file(filename="", text=""):
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
