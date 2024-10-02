b2746919b4bebc5f6e10f11ed8c3a95e11ef23df
EL274 <tresormakosso510@gmail.com>
2024-10-03 00:41:18 +0200

    modified

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """defines function return Json in object"""
-import json 
+
 
 def to_json_string(my_obj):
     """return representation json an object"""
+    import json 
     return json.dumps(my_obj)
-
