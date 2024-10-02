e57a54a56c1cd295028072c01464707c1a664643
EL274 <tresormakosso510@gmail.com>
2024-10-02 18:34:10 +0200

    write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""defines function that counts number of lines read from file"""
+"""Defines a function writes text file and returns number of characters"""
 
+def write_file(filename="", text=""):
+    """Writes text file and returns number of characters"""
 
-def number_of_lines(filename=""):
-    """returns number of lines read from text file"""
-    count = 0
-    with open(filename) as f:
-        while f.readline() is not "":
-            count += 1
-    return count
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
