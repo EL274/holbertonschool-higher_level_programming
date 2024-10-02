ee5042c49e9fad3fbd75237a0271c8a1aec9f326
EL274 <tresormakosso510@gmail.com>
2024-10-03 01:57:54 +0200

    save

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """defines function writes json representation an object"""
 
-import json
-"""write representation json an object to file"""
-
 
 def save_to_json_file(my_obj, filename):
+    """write representation json an object to file"""
+    
+    import json
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file)
