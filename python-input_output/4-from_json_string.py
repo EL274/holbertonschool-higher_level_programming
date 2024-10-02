2ab155cc48a8067be2938d28c13fa294f402a118
EL274 <tresormakosso510@gmail.com>
2024-10-03 01:34:33 +0200

    from

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""defines function returns an object from json"""
+
+
 import json
 
 def from_json_string(my_str):
+    """return an object from json"""
     return json.loads(my_str)
