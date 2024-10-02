deac5675ff58faf045c9d62ed717060d08ad2c0d
EL274 <tresormakosso510@gmail.com>
2024-10-02 15:47:38 +0200

    modified

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Defines a function that writes text to a file and returns the number of characters written.
-"""
+"""Defines a function that writes text to a file and returns the number of characters written."""
 
 def write_file(filename="", text=""):
     """
