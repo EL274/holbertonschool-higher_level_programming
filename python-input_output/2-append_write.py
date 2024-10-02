e607a5c226f636b5d620c10e2a9d7d4d40526813
EL274 <tresormakosso510@gmail.com>
2024-10-03 00:22:46 +0200

    modified

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines function to append string text file"""
 
+
 def append_write(filename="", text=""):
     """append string in text file"""
     with open(filename, "a", encoding="utf-8") as file:
