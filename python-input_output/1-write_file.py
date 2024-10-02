b35c6013769692f41dfc16cd67a69b2c1a5838f4
EL274 <tresormakosso510@gmail.com>
2024-10-02 15:47:09 +0200

    modified

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Defines a function that writes text to a file and returns  number""" 
-
+"""
+Defines a function that writes text to a file and returns the number of characters written.
+"""
 
 def write_file(filename="", text=""):
+    """
+    Writes text to a file and returns the number of characters written.
+    """
 
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
