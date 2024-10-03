782f18bd62c396420990cfb2abb07029e9d48db1
EL274 <tresormakosso510@gmail.com>
2024-10-03 02:06:53 +0200

    load

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-import json
+"""defines functions creates an object from json"""
+
 
 def load_from_json_file(filename):
+    """creates an abject from json"""
+    import json
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
 
