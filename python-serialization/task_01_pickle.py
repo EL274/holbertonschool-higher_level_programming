617e6090414f1acf7e612cd8cb7807f44445b2fb
EL274 <tresormakosso510@gmail.com>
2024-10-03 17:22:26 +0200

    task

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,15 +6,19 @@ import pickle
 
 
 class CustomObject:
+    """initialization of function"""
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
+
+        
     def display(self):
-                                                     
+        """print all of attributes"""                               
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
+
     def serialize(self, filename):
         try:
             with open(filename, 'wb') as file:
