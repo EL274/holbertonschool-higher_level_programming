f6248e655096ca6c17bb7164bb4597e89ad7a08f
EL274 <tresormakosso510@gmail.com>
2024-10-03 11:51:30 +0200

    pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """defines function creates and representing pascal's triangle"""
+
+
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
     triangle = [[1]]
 
     for i in range(1, n):
 
         prev_row = triangle[-1]
         new_row = [1]
-        
         for j in range(1, len(prev_row)):
             new_row.append(prev_row[j-1] + prev_row[j])
-        
         new_row.append(1)
-    
         triangle.append(new_row)
 
     return triangle
