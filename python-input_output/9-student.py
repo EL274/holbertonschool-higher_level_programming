e6b6b67dbe9991f2294bee024c59159f8062c6e8
EL274 <tresormakosso510@gmail.com>
2024-10-03 10:32:20 +0200

    student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,6 +1,8 @@
 class Student:
     def __init__(self, first_name, last_name, age):
         """Initialisation of new instance student"""
+
+        
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
