763d02430eec8492a1f9c1d77a84856adaf23b28
EL274 <tresormakosso510@gmail.com>
2024-10-03 11:16:06 +0200

    student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 """defines function writes class of student"""
 
-
 class Student:
-def __init__(self, first_name, last_name, age):
-        """Initialisation new class of student"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    def __init__(self, first_name, last_name, age):
+        self.first_name, self.last_name, self.age = first_name, last_name, age
 
-def to_json(self, attrs=None):
-        """returns attribute  instance"""
-        if attrs is not None and isinstance(attrs, list):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
-        else:
-            return self.__dict__
-
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
