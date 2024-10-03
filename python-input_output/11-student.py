7b183c9be466169013eb31302b0d8d3f967ad12d
EL274 <tresormakosso510@gmail.com>
2024-10-03 11:39:48 +0200

    student

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """defines function write class of student"""
 
-
 class Student:
     def __init__(self, first_name, last_name, age):
-        """Initialise les attributs de l'instance Student"""
+        """Initialisation of attribute student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retourne un dictionnaire représentant l'instance Student.
-        Si attrs est une liste de chaînes de caractères, seules les
-        clés correspondant à ces attributs sont retournées.
-        """
-        if attrs is not None and isinstance(attrs, list):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        if isinstance(attrs, list):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """
-        Remplace les attributs de l'instance par les valeurs dans le dictionnaire json.
-        """
         for key, value in json.items():
             setattr(self, key, value)
