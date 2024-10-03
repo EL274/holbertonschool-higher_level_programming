f43514adbe6aebbb4120910f90cb9f23c3430a77
EL274 <tresormakosso510@gmail.com>
2024-10-03 10:38:37 +0200

    modified

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,13 +1,15 @@
+#!/usr/bin/python3
+"""defines function writes the class of student"""
+
+
 class Student:
     def __init__(self, first_name, last_name, age):
         """Initialisation of new instance student"""
-
-        
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """Return representation dictionary instance"""
-        return self.__dict__
 
+        return self.__dict__
