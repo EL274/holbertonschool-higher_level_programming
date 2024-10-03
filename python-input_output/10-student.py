fc6e798ac182b9c63f6c82944d03e3eec9375915
EL274 <tresormakosso510@gmail.com>
2024-10-03 11:04:38 +0200

    student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,4 +1,7 @@
 class Student:
+     """defines function writes class of student"""
+
+
     def __init__(self, first_name, last_name, age):
         """Initialise une nouvelle instance de Student"""
         self.first_name = first_name
@@ -6,11 +9,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retourne la représentation sous forme de dictionnaire de l'instance.
-        
-        Si 'attrs' est une liste de chaînes de caractères, seuls les attributs
-        mentionnés dans 'attrs' sont retournés. Sinon, tous les attributs sont retournés.
-        """
+        """returns attribute  instance"""
         if attrs is not None and isinstance(attrs, list):
             return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
         else:
