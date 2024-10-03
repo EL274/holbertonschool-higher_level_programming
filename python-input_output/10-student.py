2f7c6adc6822e393d7bcb4b42602c6d881384eb1
EL274 <tresormakosso510@gmail.com>
2024-10-03 11:16:24 +0200

    student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines function writes class of student"""
 
+
 class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name, self.last_name, self.age = first_name, last_name, age
