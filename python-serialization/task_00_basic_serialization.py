5df6ecf6060ff45d238e8b6f3628053b065a599d
EL274 <tresormakosso510@gmail.com>
2024-10-03 14:31:34 +0200

    basic

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,12 +3,14 @@
 import json
 def serialize_and_save_to_file(data, filename):
 
-    
+
     """Serializes Python dictionary and saves specified file in JSON format."""
     try:
         with open(filename, 'w') as file:
             json.dump(data, file)
     except Exception as e:
+
+        
         print(f"An error occurred while saving data to {filename}: {e}")
 def load_and_deserialize(filename):
     """Loads and deserializes JSON data specified file Python dictionary."""
