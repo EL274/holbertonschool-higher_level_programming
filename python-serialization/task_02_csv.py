1804a47a8c620e0d878895345df9ec4b58224e44
EL274 <tresormakosso510@gmail.com>
2024-10-04 02:40:54 +0200

    task 02

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,22 @@
+import csv
+import json
+
+def convert_csv_to_json(csv_filename):
+    try:
+        # Ouvrir le fichier CSV et lire les données
+        with open(csv_filename, mode='r', newline='', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = [row for row in csv_reader]
+
+        # Écrire les données JSON dans un fichier
+        with open('data.json', mode='w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return True
+    except FileNotFoundError:
+        print(f"Erreur : Le fichier {csv_filename} est introuvable.")
+        return False
+    except Exception as e:
+        print(f"Erreur : {e}")
+        return False
+
