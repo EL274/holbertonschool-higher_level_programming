c8abaa69d2d66afce08370f74a7a0e811b01ae76
EL274 <tresormakosso510@gmail.com>
2024-10-04 11:28:15 +0200

    modified

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """defines function writes json data"""
+
+
 import csv
 import json
 
 def convert_csv_to_json(csv_filename):
     try:
-        # Ouvrir le fichier CSV et lire les données
         with open(csv_filename, mode='r', newline='', encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             data = [row for row in csv_reader]
-
-        # Écrire les données JSON dans un fichier
         with open('data.json', mode='w', encoding='utf-8') as json_file:
             json.dump(data, json_file, indent=4)
 
