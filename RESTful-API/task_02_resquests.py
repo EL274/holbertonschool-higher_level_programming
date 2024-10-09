9b810936a8430a9a0f8b2ec7d74c2b9b007d8479
EL274 <tresormakosso510@gmail.com>
2024-10-10 01:43:38 +0200

    try

--- RESTful-API/task_02_resquests.py
+++ RESTful-API/task_02_resquests.py
@@ -1,11 +1,11 @@
-
 #!/usr/bin/python3
 
 import csv
 import requests
 
+
 def fetch_and_print_posts():
-    """Récupère et affiche les posts de JSONPlaceholder"""
+    # Récupère et affiche les posts de JSONPlaceholder
     url = "https://jsonplaceholder.typicode.com/posts"
     try:
         response = requests.get(url)
@@ -26,7 +26,7 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
-    """Récupère et sauvegarde les posts de JSONPlaceholder dans un CSV"""
+    '# Récupère et sauvegarde les posts de JSONPlaceholder dans un CSV'
     url = "https://jsonplaceholder.typicode.com/posts"
     try:
         response = requests.get(url)
@@ -43,7 +43,6 @@ def fetch_and_save_posts():
 
     post_list = [{'id': p['id'], 'title': p['title'], 'body': p['body']}
                  for p in posts]
-    
     with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
         writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
         writer.writeheader()
