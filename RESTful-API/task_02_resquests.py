6a75bd60583457c66cd29c678cf14fa1ba371bcf
EL274 <tresormakosso510@gmail.com>
2024-10-10 01:26:49 +0200

    task

--- RESTful-API/task_02_resquests.py
+++ RESTful-API/task_02_resquests.py
@@ -1,52 +1,28 @@
 #!/usr/bin/python3
 
-
+import csv
 import requests
 
 def fetch_and_print_posts():
-    """URL de l'API JSONPlaceholder pour récupérer les posts"""
+    """Récupère et affiche les posts de JSONPlaceholder"""
     url = "https://jsonplaceholder.typicode.com/posts"
-    
-    """Envoie une requête GET pour récupérer les posts"""
     response = requests.get(url)
-    
-    """Affiche le code de statut de la réponse"""
     print(f"Status Code: {response.status_code}")
-    
-    """Si la requête a réussi (status_code == 200)"""
     if response.status_code == 200:
-        """Convertir la réponse en JSON"""
         posts = response.json()
-        
-        """Parcourir les posts et afficher les titres"""
         for post in posts:
             print(post['title'])
 
-
-import csv
-
 def fetch_and_save_posts():
-    """ URL de l'API JSONPlaceholder pour récupérer les posts"""
+    """Récupère et sauvegarde les posts de JSONPlaceholder dans un CSV"""
     url = "https://jsonplaceholder.typicode.com/posts"
-    
-    """Envoie une requête GET pour récupérer les posts"""
     response = requests.get(url)
-    
-    """Si la requête a réussi (status_code == 200)"""
     if response.status_code == 200:
-        """Convertir la réponse en JSON"""
         posts = response.json()
-        
-        """Préparer les données sous forme d'une liste de dictionnaires"""
-        post_list = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
-        
-        """Ouvrir un fichier CSV en mode écriture"""
+        post_list = [{'id': p['id'], 'title': p['title'], 'body': p['body']} 
+                     for p in posts]
         with open('posts.csv', mode='w', newline='') as file:
-            """Créer un objet DictWriter pour écrire dans le fichier CSV"""
             writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-            
-            """ Écrire l'en-tête (noms des colonnes)"""
             writer.writeheader()
-            
-            """ Écrire les données dans le fichier"""
             writer.writerows(post_list)
+
