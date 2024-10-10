0bdc5d774a065de2613de826f3a51a67bd68747a
EL274 <tresormakosso510@gmail.com>
2024-10-10 13:32:41 +0200

    modified

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,31 +4,30 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-
 """Classe qui gère les requêtes HTTP"""
+
+
 class SimpleAPIHandler(BaseHTTPRequestHandler):
-    
     """Gère les requêtes GET"""
     def do_GET(self):
         # Gérer la route principale "/"
         if self.path == "/":
-            self.send_response(200)  # Réponse HTTP 200 OK
-            self.send_header("Content-type", "text/plain")  # Définir le type de contenu
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
             self.end_headers()  # Fin des en-têtes
-            self.wfile.write(b"Hello, this is a simple API!")  # Corps de la réponse
-        
+            self.wfile.write(b"Hello, this is a simple API!")        
         # Gérer la route "/data"
         elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            # Les données à renvoyer en JSON
+            # Les données à renvoyer en Json 
             data = {
                 "name": "John",
                 "age": 30,
                 "city": "New York"
             }
-            self.wfile.write(json.dumps(data).encode())  # Convertir dict en JSON
+            self.wfile.write(json.dumps(data).encode())
         
         # Gérer la route "/status"
         elif self.path == "/status":
@@ -51,11 +50,14 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(error_data).encode())
 
 # Fonction pour démarrer le serveur HTTP
+
+
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f"Starting http server on port {port}...")
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
