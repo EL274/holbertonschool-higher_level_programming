ead2e822155f57142e4cc18cf7219efca64e2f09
EL274 <tresormakosso510@gmail.com>
2024-10-11 16:42:54 +0200

    modified

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,14 +2,14 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+class echoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             # Réponse pour la racine de l'API
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
-            self.wfile.write('Hello, this is a simple API!'.encode())
+            self.wfile.write(self.path.encode())
 
         elif self.path == '/status':
             # Réponse pour l'endpoint /status
@@ -25,15 +25,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 "age": 30,
                 "city": "New York"
             }
-            
             # Convertit les données en JSON
             json_data = json.dumps(data)
-            
             # Définit le code de réponse et les en-têtes
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            
             # Envoie les données JSON en réponse
             self.wfile.write(json_data.encode('utf-8'))
 
@@ -43,16 +40,17 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/html")
             self.end_headers()
             error_message = {
-                "error": "404 Not Found",
-                "message": "The endpoint you are trying to access does not exist."
+                "error": "404 Not Found", "message": "error"
             }
             self.wfile.write(json.dumps(error_message).encode('utf-8'))
 
+
 def main():
     PORT = 8000
-    server = HTTPServer(('', PORT), SimpleHTTPRequestHandler)
+    server = HTTPServer(('', PORT), echoHandler)
     print('server running on port %s' % PORT)
     server.serve_forever()
+
 
 if __name__ == '__main__':
     main()
