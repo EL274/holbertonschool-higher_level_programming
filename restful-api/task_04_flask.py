3844f555309064d4d7f6667703a389858c6d1bbb
EL274 <tresormakosso510@gmail.com>
2024-10-13 14:16:56 +0200

    try again

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -10,19 +10,25 @@ users = {
 }
 
 # Root endpoint
+
+
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
 
 # Get all users (JSON data)
+
+
 @app.route('/data')
 def get_users():
     return jsonify(users)
+
 
 # Get status
 @app.route('/status')
 def status():
     return "OK"
+
 
 # Get user by username
 @app.route('/users/<username>')
@@ -33,20 +39,21 @@ def get_user(username):
     return jsonify({"error": "User not found"}), 404
 
 # Add a new user (POST request)
+
+
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
     username = data.get("username")
-    
     if username in users:
         return jsonify({"error": "User already exists"}), 400
-    
     users[username] = {
         "name": data.get("name"),
         "age": data.get("age"),
         "city": data.get("city")
     }
     return jsonify({"message": "User added", "user": users[username]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
