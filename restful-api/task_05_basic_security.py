3012dd90e98e089b63c66bfe6baf7994076d1d6a
EL274 <tresormakosso510@gmail.com>
2024-10-14 15:24:32 +0200

    yep

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -57,11 +57,11 @@ def login():
 
     user = users.get(username)
     if user and check_password_hash(user['password'], password):
-                access_token = create_access_token(
+        access_token = create_access_token(
                 identity={
                     "username": username,     
-                    "role": user['role']     
-                    }     
+                    "role": user['role'] 
+                    }
                 )
 
         return jsonify(access_token=access_token), 200
