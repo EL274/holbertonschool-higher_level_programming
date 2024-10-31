b64de07e384b8ede8cbdb524ea905455e3e0a702
EL274 <tresormakosso510@gmail.com>
2024-10-31 15:23:29 +0100

    delete

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,1 @@
-#!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
-import MySQLdb
 
-if len(sys.argv) != 4:
-    print("Usage: ./0-select_states.py <username> <password> <database>")
-    sys.exit(1)
-
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
