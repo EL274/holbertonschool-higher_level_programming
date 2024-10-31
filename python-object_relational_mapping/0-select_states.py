afdf24f82b33844289615a92bc7064f2d756774a
EL274 <tresormakosso510@gmail.com>
2024-10-31 15:14:17 +0100

    try

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,8 +10,4 @@ if len(sys.argv) != 4:
     print("Usage: ./0-select_states.py <username> <password> <database>")
     sys.exit(1)
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
